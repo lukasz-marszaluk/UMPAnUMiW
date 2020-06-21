84decc557e8b27187fef30cdb8cfc666157c82cf
WolnyDominik <wolny.dominik@outlook.com>
2020-06-21 15:54:05 +0200

    Code style, add safegruards

--- main.cpp
+++ main.cpp
@@ -12,9 +12,8 @@ int main(int argc, char **argv)
     params input_params;
     image *photo;
     document *doc;
-    
-    
-    input_params = read_arguments (argc, argv);
+
+    input_params = read_arguments(argc, argv);
 
     if (input_params.print_help)
         print_help(argv[0]);
@@ -26,12 +25,28 @@ int main(int argc, char **argv)
     }
 
     if (input_params.read_photo_from_file)
+    {
         photo = new image(input_params.input_file);
+
+        if (photo->last_result != OK)
+        {
+            print_message("Error reading photo\n");
+            return -1;
+        }
+    }
     else
+    {
         photo = take_photo();
-    
+
+        if (photo->last_result != OK)
+        {
+            print_message("Error taking photo\n");
+            return -1;
+        }
+    }
+
     if (input_params.save_preview)
-        photo->save ("./preview.jpg");
+        photo->save("./preview.jpg");
 
     doc = new document(photo);
 
@@ -40,17 +55,16 @@ int main(int argc, char **argv)
         doc->recognize_document();
         doc->stretch_document();
     }
-    
+
     if (!input_params.disable_color_correction)
         doc->improve_colors();
 
     doc->get_document_image()->save(input_params.output_file);
-    
+
     if (input_params.read_photo_from_file)
         delete photo;
     else
-        delete_photo (photo);
-    
+        delete_photo(photo);
 
     print_message("Done\n");
     return 0;
